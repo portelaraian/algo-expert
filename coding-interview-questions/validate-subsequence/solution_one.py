2ff0e08fd3ed22fb3c0741f970b98de13ef4c2eb
Raian Portela <raianportela@hotmail.com>
2021-02-27 19:24:22 -0300

    Update solution_one.py

--- coding-interview-questions/validate-subsequence/solution_one.py
+++ coding-interview-questions/validate-subsequence/solution_one.py
@@ -3,7 +3,7 @@ def isValidSubsequence(array, sequence):
 	
 	j = 0 # Sequence index
 	for i in range(len(array)):
-		# Ensuring that will not have a IndexError
+		# Ensuring that will not have an IndexError
 		if j <= (len(sequence) -1):
 			if array[i] == sequence[j]:
 				j += 1
